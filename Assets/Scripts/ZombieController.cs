249439a0fef86dd1c394db91ea16cda31fe70fa4
Ondrej Sykora <mail@ondrejsykora.com>
2020-04-18 09:37:51 +0200

    Added a blocky environment + first zombie.

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    public Vector2 destination;
+
+    public float randomMovementDestinationRadius = 1.0f;
+
+    public float destinationResetRadius = 0.1f;
+
+    public float movementSpeed = 1.0f;
+
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // TODO: Move to destination if possible.
+    }
+
+    private bool IsAtDestination()
+    {
+        var distance = (rigidBody.position - destination).magnitude;
+        return distance <= destinationResetRadius;
+    }
+
+    private void UpdateDestination()
+    {
+        var destinationDelta = randomMovementDestinationRadius * Random.insideUnitCircle;
+        destination = rigidBody.position + destinationDelta;
+    }
+}
