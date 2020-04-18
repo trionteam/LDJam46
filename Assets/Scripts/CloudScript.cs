c7b8839fdfbee4ba1ad0631c3738cd9fa09505f6
pcmaster@koinbahd.com <pcmaster@koinbahd.com>
2020-04-18 16:25:25 +0200

    Added clouds on spray

---
+++ Assets/Scripts/CloudScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+	public float LifeTime = 5;  // seconds
+	public float LifeLengthRandomness = 0.25f;
+	public float FadeLastSeconds = 0.5f;    // number of seconds to fade after it dies
+	private Vector3 velocity = Vector3.zero;
+
+	private float lifeLeft = 0;
+	private SpriteRenderer spriteRenderer;
+	private float rot = 1;
+
+	public void SetVelocity(Vector2 vel)
+	{
+		velocity = vel;
+		rot = Random.Range(0, 2) == 0 ? -1.0f : 1.0f;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		lifeLeft = LifeTime + Random.Range(-1.0f, 1.0f) * LifeLengthRandomness;
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+
+		Color c = spriteRenderer.color;
+		c.a = 0;
+		spriteRenderer.color = c;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position += velocity * Time.deltaTime;
+		transform.Rotate(Vector3.forward, rot * Time.deltaTime);
+
+		Color c = spriteRenderer.color;
+		lifeLeft -= Time.deltaTime;
+		if (lifeLeft <= 0)
+		{
+			c.a = (FadeLastSeconds + lifeLeft) / FadeLastSeconds;
+			if (lifeLeft <= -FadeLastSeconds)
+			{
+				Destroy(gameObject);
+				return;
+			}
+		}
+		else
+		{
+			c.a = Mathf.Clamp(c.a + Time.deltaTime * FadeLastSeconds, 0, 1);
+		}
+		spriteRenderer.color = c;
+
+	}
+}
--- Assets/Scripts/RotatingSpray.cs
+++ Assets/Scripts/RotatingSpray.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class RotatingSpray : MonoBehaviour
 {
 	public GameObject Cone;
+	public GameObject CloudPrefab;
 	public float Speed = 100;   // degrees per second
 	public float Radius = 3;    // radius in units
-	public int layerMask = 0x8;
+	public int BurstSize = 10;
+
+	public float SprayCooldownTime = 1;
+	private float sprayCooldown = 1;
 
     // Start is called before the first frame update
     void Start()
     {
 		Debug.Assert(null != Cone);
+		Debug.Assert(null != CloudPrefab);
 	}
 
 	Vector3 GetDir()
@@ -28,35 +33,62 @@ public class RotatingSpray : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Cone.transform.Rotate(Vector3.forward, Speed * Time.deltaTime);
-
 		// TODO - don't set every frame
 		float r = Radius * 2;
 		Cone.transform.localScale = new Vector3(r, r, r);
+		sprayCooldown -= Time.deltaTime;
 
-		DebugDraw();
-
-	}
-
-	void DebugDraw()
-	{
-		Vector3 fwd = GetDir() * Radius;
-		Quaternion q45 = Quaternion.AngleAxis(45, GetAxis());
-		Quaternion q_45 = Quaternion.AngleAxis(-45, GetAxis());
-		Vector3 right = q45 * fwd;
-		Vector3 left = q_45 * fwd;
-		Debug.DrawLine(transform.position, transform.position + fwd, Color.red);
-		Debug.DrawLine(transform.position, transform.position + right, Color.red);
-		Debug.DrawLine(transform.position, transform.position + left, Color.red);
-
-		// TODO - just a debug, that random is bad, need a "arc query", will figure out later :)
-		float rndAngle = Random.Range(-45.0f, 45.0f);
-		Vector3 rndDir = Quaternion.AngleAxis(rndAngle, GetAxis()) * fwd;
-		RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, rndDir, Radius, layerMask);
-
-		if (hitInfo)
+		//DebugDraw();
+		Collider2D col2 = Cone.GetComponent<Collider2D>();
+		List<Collider2D> colliders = new List<Collider2D>();
+		col2.GetContacts(colliders);
+		if (colliders.Count > 0)
 		{
-			Debug.DrawLine(transform.position, hitInfo.point, Color.green);
+			Vector3 colPos = colliders[0].transform.position;
+			Vector2 toCollider = (colPos - Cone.transform.position);
+			Cone.transform.LookAt(Cone.transform.position + Vector3.forward, toCollider);
+
+			if (sprayCooldown <= 0)
+			{
+				// shoot!
+				sprayCooldown = SprayCooldownTime;
+
+				Vector3 pos = transform.position;
+				for (uint i = 0; i < BurstSize; ++i)
+				{
+					Vector3 toColliderNorm = (colPos - pos).normalized;
+					GameObject g = GameObject.Instantiate(CloudPrefab, pos, Quaternion.identity);
+					g.GetComponent<CloudScript>().SetVelocity(toColliderNorm);
+					pos += (new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized * 0.05f + toColliderNorm * 0.1f);
+				}
+			}
+		}
+		else
+		{
+			Cone.transform.Rotate(Vector3.forward, Speed * Time.deltaTime);
 		}
 	}
+
+
+	//void DebugDraw()
+	//{
+	//	Vector3 fwd = GetDir() * Radius;
+	//	Quaternion q45 = Quaternion.AngleAxis(45, GetAxis());
+	//	Quaternion q_45 = Quaternion.AngleAxis(-45, GetAxis());
+	//	Vector3 right = q45 * fwd;
+	//	Vector3 left = q_45 * fwd;
+	//	Debug.DrawLine(transform.position, transform.position + fwd, Color.red);
+	//	Debug.DrawLine(transform.position, transform.position + right, Color.red);
+	//	Debug.DrawLine(transform.position, transform.position + left, Color.red);
+
+	//	// TODO - just a debug, that random is bad, need a "arc query", will figure out later :)
+	//	float rndAngle = Random.Range(-45.0f, 45.0f);
+	//	Vector3 rndDir = Quaternion.AngleAxis(rndAngle, GetAxis()) * fwd;
+	//	RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, rndDir, Radius, layerMask);
+
+	//	if (hitInfo)
+	//	{
+	//		Debug.DrawLine(transform.position, hitInfo.point, Color.green);
+	//	}
+	//}
 }
