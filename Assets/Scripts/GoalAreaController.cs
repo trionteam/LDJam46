cfe655d083746f3bf2ad547e1ee495da0d1b717c
Ondrej Sykora <mail@ondrejsykora.com>
2020-04-18 23:37:11 +0200

    Added a scene for the tutorial level + a controller for the goal area. No functionality for level completion really implemented.

---
+++ Assets/Scripts/GoalAreaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalAreaController : MonoBehaviour
+{
+    public bool isVictory = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var zombie = collision.GetComponentInParent<ZombieController>();
+        if (zombie != null && zombie.isActiveAndEnabled)
+        {
+            isVictory = true;
+            // TODO: Victory!
+        }
+    }
+}
