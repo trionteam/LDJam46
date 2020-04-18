e1f67e26e3763e643e8e0d7bc17ab031c96bd35c
Ondrej Sykora <mail@ondrejsykora.com>
2020-04-18 22:19:38 +0200

    Escape quits the game.

--- Assets/Scripts/ZombieControls.cs
+++ Assets/Scripts/ZombieControls.cs
@@ -17,9 +17,13 @@ public class ZombieControls : MonoBehaviour
         Debug.Assert(mainCamera != null);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         if (Input.GetMouseButtonUp(0))
         {
             var clickPosition3d = mainCamera.ScreenToWorldPoint(Input.mousePosition);
