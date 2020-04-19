d0ae91e324099d07e10d937764f82070eecc299e
Ondrej Sykora <mail@ondrejsykora.com>
2020-04-19 14:08:02 +0200

    Added a spray spraying in a fixed direction.

---
+++ Assets/Scripts/FixedSpray.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedSpray : MonoBehaviour
+{
+    public GameObject CloudPrefab;
+    public Transform Cone;
+
+    public float SpraySpeed = 2.0f;
+    public float SprayPeriod = 5.0f;
+    public int BurstSize = 10;
+    public float BurstSpreadDegrees = 45.0f;
+
+    private float LastBurstTime;
+
+    private void Awake()
+    {
+        Debug.Assert(CloudPrefab != null);
+    }
+
+    private void Start()
+    {
+        LastBurstTime = Time.time;
+    }
+
+    Vector3 GetDirection()
+    {
+        return Cone.transform.up;
+    }
+
+    void Update()
+    {
+        if (Time.time > LastBurstTime + SprayPeriod)
+        {
+            var pos = transform.position;
+            for (int i = 0; i < BurstSize; ++i)
+            {
+                var spreadAngle = Random.Range(-BurstSpreadDegrees / 2.0f, BurstSpreadDegrees / 2.0f);
+                Vector3 direction = Quaternion.Euler(0.0f, 0.0f, spreadAngle) * GetDirection().normalized;
+                GameObject g = GameObject.Instantiate(CloudPrefab, pos, Quaternion.identity);
+                g.GetComponent<CloudScript>().Init(direction * SpraySpeed);
+                pos += (new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized * 0.05f + direction * 0.1f);
+            }
+
+            // TODO - sound wildcards
+            SoundMgr.Instance?.Play($"spray_{Random.Range(0, 2)}");
+            LastBurstTime = Time.time;
+        }
+    }
+}
