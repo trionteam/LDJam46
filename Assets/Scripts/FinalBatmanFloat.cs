73a14b3a3cbaf9acb5e223a249828d80cb53fab8
Ondrej Sykora <mail@ondrejsykora.com>
2020-04-20 22:16:10 +0200

    Tuning the final scene.

--- Assets/Scripts/FinalBatmanFloat.cs
+++ Assets/Scripts/FinalBatmanFloat.cs
@@ -7,17 +7,23 @@ public class FinalBatmanFloat : MonoBehaviour
 	public float FloatAmp = 5;
 	public float FloatSpeed = 5;
 	public float SpeedRnd = 0.5f;
-	private Vector3 basePos;
+    //private Vector3 basePos;
+    private float baseY = 0.0f;
 	private float actualSpeed = 1;
 
 	void Start()
 	{
-		basePos = transform.localPosition;
+        var rectTransform = GetComponent<RectTransform>();
+        baseY = rectTransform.anchoredPosition.y;
 		actualSpeed = FloatSpeed * Random.Range(1.0f - SpeedRnd, 1.0f + SpeedRnd);
 	}
 
 	void Update()
 	{
-		transform.localPosition = basePos + transform.up * Mathf.Sin(Time.time * actualSpeed) * FloatAmp;
-	}
+        //transform.localPosition = basePos + transform.up * Mathf.Sin(Time.time * actualSpeed) * FloatAmp;
+        var rectTransform = GetComponent<RectTransform>();
+        var pos = rectTransform.anchoredPosition;
+        pos.y = baseY + Mathf.Sin(Time.time * actualSpeed) * FloatAmp;
+        rectTransform.anchoredPosition = pos;
+    }
 }
