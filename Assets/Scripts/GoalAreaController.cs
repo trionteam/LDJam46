872b19bfb9ad7fe8d5ae61f4e05df8fa29de52c1
Ondrej Sykora <mail@ondrejsykora.com>
2020-04-20 19:47:36 +0200

    Zombies reaching the goal area leave the screen + controls are disabled when this happens.

--- Assets/Scripts/GoalAreaController.cs
+++ Assets/Scripts/GoalAreaController.cs
@@ -6,6 +6,9 @@ public class GoalAreaController : MonoBehaviour
 {
     public LevelController levelController;
 
+    // All zombies that get to the goal area will move towards this place.
+    public Transform zombieDestination;
+
     private void Awake()
     {
         if (levelController == null)
@@ -13,6 +16,7 @@ public class GoalAreaController : MonoBehaviour
             levelController = GameObject.FindGameObjectWithTag("Managers")?.GetComponent<LevelController>();
         }
         Debug.Assert(levelController != null);
+        Debug.Assert(zombieDestination != null);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -21,6 +25,14 @@ public class GoalAreaController : MonoBehaviour
         if (zombie != null && zombie.isActiveAndEnabled)
         {
             levelController.GoalAreaReached();
+            zombie.AssignDestination(zombieDestination.position);
+
+            // Disable the zombifier to prevent the zombie from healing.
+            var zombifier = zombie.GetComponent<Zombifiable>();
+            if (zombifier != null)
+            {
+                zombifier.enabled = false;
+            }
         }
     }
 }
