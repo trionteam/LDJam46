ca3e8ae85f1d7457c8aad905c28bb6be2504c7ac
Ondrej Sykora <mail@ondrejsykora.com>
2020-04-20 17:06:13 +0200

    Added bits of new graphics. Made things wobble instead of floating.

---
+++ Assets/Scripts/WobbleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WobbleController : MonoBehaviour
+{
+    Quaternion originalRotation;
+
+    public float Amplitude = 0.05f;
+    public float WobbleSpeed = 5;
+    public float SpeedRnd = 0.25f;
+    private float actualSpeed = 1;
+
+    void Start()
+    {
+        originalRotation = transform.rotation;
+        actualSpeed = WobbleSpeed * Random.Range(1.0f - SpeedRnd, 1.0f + SpeedRnd);
+    }
+
+    void Update()
+    {
+        var wobble = Quaternion.Euler(0.0f, 0.0f, Amplitude * Mathf.Sin(Time.time * actualSpeed));
+        transform.rotation = wobble * originalRotation;
+    }
+}
