bdd240999f98b2a95c9abcd40ea99799126a2d3b
pcmaster-sk <pcmaster@koinbahd.com>
2020-04-20 20:43:59 +0200

    Added final scene (with a pig as we don't have the planet yet)

---
+++ Assets/Scripts/FinalBatmanFloat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBatmanFloat : MonoBehaviour
+{
+	public float FloatAmp = 5;
+	public float FloatSpeed = 5;
+	public float SpeedRnd = 0.5f;
+	private Vector3 basePos;
+	private float actualSpeed = 1;
+
+	void Start()
+	{
+		basePos = transform.localPosition;
+		actualSpeed = FloatSpeed * Random.Range(1.0f - SpeedRnd, 1.0f + SpeedRnd);
+	}
+
+	void Update()
+	{
+		transform.localPosition = basePos + transform.up * Mathf.Sin(Time.time * actualSpeed) * FloatAmp;
+	}
+}
---
+++ Assets/Scripts/FinalScreenController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalScreenController : MonoBehaviour
+{
+	public void ExitToMenu()
+	{
+		SceneManager.LoadScene("Scenes/MainMenu");
+	}
+}
