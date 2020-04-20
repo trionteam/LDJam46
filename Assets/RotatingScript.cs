457efecbd232324af88c91cd4a6eb9948c33aef1
pcmaster-sk <pcmaster@koinbahd.com>
2020-04-20 20:57:24 +0200

    Final scene rotating

---
+++ Assets/RotatingScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingScript : MonoBehaviour
+{
+	public float RotSpeed = 10;
+
+    void Update()
+    {
+		transform.Rotate(Vector3.forward, Time.deltaTime * RotSpeed);
+    }
+}
