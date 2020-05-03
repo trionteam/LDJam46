9739c572e8a45972ef1aaa5f76e6ae7dd6774497
Ondrej Sykora <mail@ondrejsykora.com>
2020-05-03 21:15:05 +0200

    More tuning of the aspect-ratio-independent and now also zoom-enabled camera.

--- Assets/Scripts/MainCameraScaling.cs
+++ Assets/Scripts/MainCameraScaling.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class MainCameraScaling : MonoBehaviour
 {
     static readonly float TargetAspectRatio = 16.0f / 9.0f;
-    static readonly float OriginalCameraSize = 5.0f;
+
+    public Transform blockerUp;
+    public Transform blockerDown;
+    public Transform blockerLeft;
+    public Transform blockerRight;
+
     new Camera camera = null;
+
+    public float originalCameraSize = 0.0f;
 
     private void Awake()
     {
         camera = GetComponent<Camera>();
+        Debug.Assert(camera != null);
+
+        Debug.Assert(blockerUp != null);
+        Debug.Assert(blockerDown != null);
+        Debug.Assert(blockerLeft != null);
+        Debug.Assert(blockerRight != null);
+    }
+
+    private void Start()
+    {
+        if (originalCameraSize == 0.0f)
+        {
+            originalCameraSize = camera.orthographicSize;
+        }
     }
 
     void Update()
@@ -20,11 +41,17 @@ public class MainCameraScaling : MonoBehaviour
         // Screen is wider than we want. We're fine.
         if (screenAspect >= TargetAspectRatio)
         {
-            camera.orthographicSize = OriginalCameraSize;
-            return;
+            camera.orthographicSize = originalCameraSize;
+        }
+        else
+        {
+            var scaling = TargetAspectRatio / screenAspect;
+            camera.orthographicSize = scaling * originalCameraSize;
         }
 
-        var scaling = TargetAspectRatio / screenAspect;
-        camera.orthographicSize = scaling * OriginalCameraSize;
+        blockerUp.position = new Vector3(0.0f, originalCameraSize + blockerUp.localScale.y / 2.0f, blockerUp.position.z);
+        blockerDown.position = new Vector3(0.0f, -originalCameraSize - blockerDown.localScale.y / 2.0f, blockerDown.position.z);
+        blockerLeft.position = new Vector3(-originalCameraSize * TargetAspectRatio - blockerLeft.localScale.x / 2.0f, 0.0f, blockerLeft.position.z);
+        blockerRight.position = new Vector3(originalCameraSize * TargetAspectRatio + blockerRight.localScale.x / 2.0f, 0.0f, blockerRight.position.z);
     }
 }
