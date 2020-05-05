f65ff22e06df0230b409e831de216ae1b16cc153
Ondrej Sykora <mail@ondrejsykora.com>
2020-05-06 00:47:38 +0200

    Added a pressure plate activated when someone steps on it.

--- Assets/Scripts/Layers.cs
+++ Assets/Scripts/Layers.cs
@@ -1,4 +1,5 @@
 ﻿public static class Layers
 {
+    public static readonly int Default = 0;
     public static readonly int Zombies = 8;
 }
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Sprite pressedSprite;
+    public Sprite depressedSprite;
+
+    public bool IsPressed
+    {
+        get; private set;
+    }
+
+    private Collider2D plateCollider;
+    new private SpriteRenderer renderer;
+
+    private void Awake()
+    {
+        plateCollider = GetComponent<Collider2D>();
+        Debug.Assert(plateCollider != null);
+
+        renderer = GetComponent<SpriteRenderer>();
+        Debug.Assert(renderer != null);
+
+        Debug.Assert(pressedSprite != null);
+        Debug.Assert(depressedSprite != null);
+    }
+
+    private void Start()
+    {
+        IsPressed = false;
+    }
+
+    void Update()
+    {
+        IsPressed = false;
+        var candidates = new Collider2D[64];
+        var filter = new ContactFilter2D();
+        filter.SetLayerMask(1 << Layers.Default);
+        var numCandidates = plateCollider.OverlapCollider(filter, candidates);
+        for (int i = 0; i < numCandidates; ++i)
+        {
+            if (candidates[i].gameObject.layer != Layers.Default) continue;
+            // TODO(ondrasej): We need a better component for characters that can press the plate.
+            var candidate = candidates[i].GetComponentInParent<ZombieController>();
+            if (candidate != null)
+            {
+                IsPressed = true;
+                break;
+            }
+        }
+        renderer.sprite = IsPressed ? pressedSprite : depressedSprite;
+    }
+}
